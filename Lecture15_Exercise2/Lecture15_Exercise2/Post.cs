a097e25064b69d5d6a567298680ffc1d0defe3fe
Michael Miles <boat081062@gmail.com>
2016-04-28 06:07:34 -0500

    Creating the Post class and contructor

--- Lecture15_Exercise2/Lecture15_Exercise2/Post.cs
+++ Lecture15_Exercise2/Lecture15_Exercise2/Post.cs
@@ -1,7 +1,18 @@
+using System;
+
 namespace Lecture15_Exercise2
 {
     public class Post
     {
-        public int votes { get; set; }
+        public int Votes { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Description { get; set; }
+
+        public Post(string title)
+        {
+            Votes = 0;
+            Title = title;
+        }
     }
 }

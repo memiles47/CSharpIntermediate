5624f480dfb267a82e92b9499617b474eebd4129
Michael E Miles <michael@miles-me.net>
2016-05-06 03:04:58 -0500

    Add instructions

--- StackExercise/StackExercise/Program.cs
+++ StackExercise/StackExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main()
         {
+            //Push three objects
 
+            //Pop three objects and print each as they are poped
+
+            //Clear has no fucntion as the pop deleted them
         }
     }
 }

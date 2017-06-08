85ef77211da90b9f27a5220cba9af1ada5c125f8
Michael E Miles <michael@miles-me.net>
2017-06-08 12:01:09 -0500

    Demonstration of Access modifiers - private and introduction to Encapsulation

---
+++ AccessModifiers/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.SetBirthdate(new DateTime(1962, 8, 10));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}

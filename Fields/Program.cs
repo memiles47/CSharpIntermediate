71848fdaefa6846247cd5c6f30d4e6a06b2a331e
Michael E Miles <michael@miles-me.net>
2017-06-08 06:27:09 -0500

    New training lesson from Mosh

---
+++ Fields/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(290203, "Michael Miles");
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+}

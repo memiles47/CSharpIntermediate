4b31ca8c59a1ec1066c276dd9de58d2bb5bc38f6
Michael E Miles <michael@miles-me.net>
2017-06-08 15:06:11 -0500

    Properties Demonstrations

---
+++ Properties/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        //Declare auto implemented properties at the top
+        public string Name { get; private set; }
+        public DateTime Birthdate { get; private set; }
+
+        //Horizontal seperator and then the constructor(s)
+        public Person(DateTime birthdate, string name)
+        {
+            Birthdate = birthdate;
+            Name = name;
+        }
+
+        //Horizontal seperator and then calculated properties
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = Convert.ToInt32(Math.Floor(timeSpan.TotalDays / 365));
+                return years;
+            }
+        }
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main()
+        {
+            var person = new Person(new DateTime(1962, 7, 10), "Michael");
+            Console.WriteLine($"Hi, my name is {person.Name} and I am {person.Age} years old.");
+        }
+    }
+}

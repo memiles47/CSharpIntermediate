8f9b86dc6148892cd25463c46f323f20a0a978f3
Michael E Miles <michael@miles-me.net>
2017-06-12 12:15:35 -0500

    Add try catch for thrown exception for tryng to start the stopwatch when it was currently running

---
+++ StopWatch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            var stopWatch = new StopWatch();
+            var running = true;
+
+            while (running)
+            {
+                Console.Write("Enter 'Start', 'Stop' or 'Quit': ");
+                var selection = Console.ReadLine();
+
+                switch (selection)
+                {
+                    case "start":
+                        try
+                        {
+                            stopWatch.Start();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Exception {e} thrown");
+                        }
+                        break;
+
+                    case "stop":
+                        Console.WriteLine(stopWatch.Stop() + "\n\n");
+                        break;
+
+                    case "quit":
+                        running = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private bool _timerRunning;
+        private TimeSpan _elapsedTime;
+
+        public void Start()
+        {
+            if (!_timerRunning)
+            {
+                _startTime = DateTime.Now;
+                _timerRunning = true;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public string Stop()
+        {
+            if (_timerRunning)
+            {
+                _stopTime = DateTime.Now;
+                _timerRunning = false;
+                _elapsedTime = _stopTime - _startTime;
+                return _elapsedTime.ToString();
+            }
+            else
+            {
+                return "Stopwatch is not currently running.\n\n";
+            }
+        }
+    }
+}

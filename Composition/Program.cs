4c20abefc92b54a4e66ed4dc6ac7fc22532a0d7d
Michael E Miles <michael@miles-me.net>
2017-06-13 14:39:29 -0500

    Add comment as to what this is demonstrating

---
+++ Composition/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Composition
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Demonstrates the use of Composition as opposed to inheritance
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}

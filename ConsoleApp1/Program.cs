d3c0a05af4c82ef5d242335a6999bc01699917bb
Michael E Miles <michael@miles-me.net>
2017-06-15 03:15:14 -0500

    Employing an idea I had while sleeping last night

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace CSharpIntermediate
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            /*
+             * This is where I will put the information needed
+             * for what is happening with each of the solutions
+             * I create for each of the sections/videos in my Udemy
+             * classes.
+             */
         }
     }
 }

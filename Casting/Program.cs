f7b7a15ab265e00b711c51cb299370b952b36881
Michael E Miles <michael@miles-me.net>
2017-06-20 03:57:41 -0500

    Commit from work

--- Casting/Program.cs
+++ Casting/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main()
         {
+            //Upcasting
+            Text text = new Text();
+            Shape shape = text;
+        
+            //Look at what is availble in shape. -vs- text.
+
 
         }
     }
